42043ccfc3e3ffe3fe06f3df8438e50af05a22f2
Simon de Haan <simon@soocial.com>
2009-09-26 15:16:57 +0200

    Removing old TherapyEdge cron.py implementation, leaving cron Django app as it it and fixing some license typos incorrectly stating 'cron'

--- apps/general/jquery/media/js/jquery.filteredselect.js
+++ apps/general/jquery/media/js/jquery.filteredselect.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2009, Jurie-Jan Botha
 //
-// This file is part of the 'cron' Django application.
+// This file is part of the 'jquery' Django application.
 //
 // 'jquery' is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
