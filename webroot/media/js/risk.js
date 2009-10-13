5235662fafb73a1b427c7f2fdcb50e4b2954b922
Simon de Haan <simon@soocial.com>
2009-10-13 18:12:20 +0200

    work in progress, not letting the HD ruin my fun a second time

--- webroot/media/js/risk.js
+++ webroot/media/js/risk.js
@@ -1,12 +1,58 @@
 BookingTool = {
 	init: function() {
+		// get all risk inputs
+		inputs = $('input.risk-field');
+		inputs.each(function(idx) {
+			BookingTool.calendars[idx] = {};
+			BookingTool.calendars[idx]['input'] = this;
+			
+			// add the calendar container
+			$(this).after('<div id="risk-calendar-box-' + idx + '"></div>');
+			
+			// add the calendar links
+			$(this).after('<a href="#" id="risk-calendar-' + idx + '">' + 
+							'<img src="/media/img/admin/icon_calendar.gif">' + 
+						'</a>');
+		});
 		
+		// apply change event listeners to all
+		inputs.change(function(event) {
+			// input element is piggy backed in event.target
+			check_risk_for_field($(event.target));
+		});
+		
+		// collect img anchors, use those for positioning, easier than
+		// using the anchors themselves
+		img_anchors = $('a[id^=risk-calendar] img');
+		img_anchors.each(function(idx) {
+			// the anchor is the parent
+			anchor = $(this).parent();
+			// and stores the id of the calendar
+			calendar_id = parseInt(anchor.attr("id").split("risk-calendar-")[1], 10);
+			
+			BookingTool.calendars[calendar_id]["anchor"] = anchor;
+			BookingTool.calendars[calendar_id]["img"] = this;
+			
+			$(this).click(function() {
+				BookingTool.show_calendar(calendar_id, this);
+			});
+			// stop bubbling
+			return false;
+		});
 	},
 	
 	calendars: [],
 	
 	show_calendar: function(calendar_id, img) {
 		
+		position = $(img).position();
+		
+		container = $('risk-calendar-box-' + calendar_id);
+		container.html('<p><img src="/static/images/risk.loading.gif"> Loading ... </p>');
+		container.css('position', 'absolute');
+		container.load('/bookingtool/calendar/today.html', '', function(resp, status, req) {
+			
+		});
 	},
 	
 	check_risk_for_field: function(input) {
