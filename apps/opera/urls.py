01c6e96a327e5bb2958ebccb2deadea6ec3028dc
Simon de Haan <simon@soocial.com>
2009-10-28 09:52:22 +0200

    workaround for missing trailing slash in Opera

--- apps/opera/urls.py
+++ apps/opera/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 from opera import views
 
 urlpatterns = patterns('',
+    (r'receipt$', views.receipt, {}, 'sms-receipt'),
     (r'receipt/$', views.receipt, {}, 'sms-receipt'),
     )
