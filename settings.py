4c770a2b7e411a10bb13dfcba2f8d9245e8d8c79
Simon de Haan <simon@soocial.com>
2010-01-12 10:49:39 +0200

    starting on a core txtalert application, separate from `apps`

--- settings.py
+++ settings.py
@@ -1,1 +1,6 @@
+# default environment is the development environment
+# specify a different settings with manage.py's --settings option
+#
+# e.g.  python manage.py --settings=environments.production
+#       python manage.py --settings=environments.testing
 from environments.development import *
