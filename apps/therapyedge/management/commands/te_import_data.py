a1cb80dfa803e5080a2d3ca2ab109e9d7d7548e5
Simon de Haan <simon@soocial.com>
2010-11-02 15:53:19 +0200

    adding exception handling for non-existent clinics

--- apps/therapyedge/management/commands/te_import_data.py
+++ apps/therapyedge/management/commands/te_import_data.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from general.settings.models import Setting
 from therapyedge.importer import Importer
 from core.models import Clinic
+from xml.parsers.expat import ExpatError
+import sys, traceback
 
 class Command(BaseCommand):
     help = "Can be run as a cronjob or directly to send import TherapyEdge data."
@@ -27,9 +29,15 @@ class Command(BaseCommand):
             # until 30 days later
             until = midnight + timedelta(days=30)
             print clinic.name, 'from', since, 'until', until
-            for key, value in importer.import_all_changes(
-                    clinic, 
-                    since=since,
-                    until=until
-                ).items():
-                print "\t%s: %s" % (key, value)
+            try:
+                for key, value in importer.import_all_changes(
+                        clinic, 
+                        since=since,
+                        until=until
+                    ).items():
+                    print "\t%s: %s" % (key, value)
+            except ExpatError, e:
+                print "Exception during processing XML for clinic ", clinic
+                traceback.print_exc()
+            
+                
