5e01d16787eacec5957e3bb2a9106c75a9ab37fb
Simon de Haan <simon@praekeltfoundation.org>
2011-01-21 20:47:06 +0200

    moving into package

--- settings.py
+++
@@ -1,6 +0,0 @@
-# default environment is the development environment
-# specify a different settings with manage.py's --settings option
-#
-# e.g.  python manage.py --settings=environments.production
-#       python manage.py --settings=environments.testing
-# from environments.development import *
