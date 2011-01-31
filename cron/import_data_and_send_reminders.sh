1396431abc15bcfe1ac92f571f8c0353d8b7244e
Simon de Haan <simon@praekeltfoundation.org>
2011-01-31 11:18:15 +0200

    adding cron job helpers

---
+++ cron/import_data_and_send_reminders.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /var/praekelt/txtalert/production/current/txtalert && \
+    source ve/bin/activate && \
+        ./manage.py te_import_data --settings=environments.production && \
+        ./manage.py te_send_reminders --settings=environments.production && \
+    deactivate
