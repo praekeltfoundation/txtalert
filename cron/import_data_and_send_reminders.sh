2c9ab0fc97367aeab67043511fb5985d8e79b249
Simon de Haan <simon@praekeltfoundation.org>
2011-02-08 11:35:55 +0200

    updated cron script to email results to dev

--- cron/import_data_and_send_reminders.sh
+++ cron/import_data_and_send_reminders.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/praekelt/txtalert/production/current/txtalert && \
     source ve/bin/activate && \
-        ./manage.py te_import_data --settings=environments.production && \
-        ./manage.py te_send_reminders --settings=environments.production && \
+        ./manage.py te_import_data --settings=environments.production | mail -s "TxtAlert TE import on `date`" dev@praekeltfoundation.org && \
+        ./manage.py te_send_reminders --settings=environments.production | mail -s "TxtAlert TE reminders on `date`" dev@praekeltfoundation.org && \
     deactivate
