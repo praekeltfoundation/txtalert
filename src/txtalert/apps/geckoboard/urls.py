fbb58e329a20619dc3346e2af2873011ff5f3c33
Simon de Haan <simon@praekeltfoundation.org>
2011-03-23 11:56:52 +0200

    pcms received

--- src/txtalert/apps/geckoboard/urls.py
+++ src/txtalert/apps/geckoboard/urls.py
@@ -4,4 +4,5 @@ from txtalert.apps.geckoboard import views
 urlpatterns = patterns('',
     (r'^patient_count/$', views.patient_count),
     (r'^smss_sent/$', views.smss_sent),
+    (r'^pcms_received/$', views.pcms_received),
 )
--- src/txtalert/apps/geckoboard/views.py
+++ src/txtalert/apps/geckoboard/views.py
@@ -1,6 +1,6 @@
 from django_geckoboard.decorators import number_widget, line_chart
 from datetime import datetime, timedelta, date
-from txtalert.core.models import Patient
+from txtalert.core.models import Patient, PleaseCallMe
 from txtalert.apps.gateway.models import SendSMS
 
 @number_widget
@@ -29,3 +29,17 @@ def smss_sent(request):
                             delivery__gte=last_week_end,
                         )
     return (this_week_sms.count(), last_week_sms.count())
+
+@number_widget
+def pcms_received(request):
+    last_week_start = datetime.now() - timedelta(weeks=2)
+    last_week_end = datetime.now() - timedelta(weeks=1)
+    pcms = PleaseCallMe.objects
+    last_week_pcms = pcms.filter(
+                            timestamp__gte=last_week_start,
+                            timestamp__lte=last_week_end)
+    this_week_pcms = pcms.filter(
+                            timestamp__gte=last_week_end,
+                        )
+    return (this_week_pcms.count(), last_week_pcms.count())
+    
