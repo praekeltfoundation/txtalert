4c86e66eb2b1c1da6c04414971d6d3176ee4e005
Simon de Haan <simon@praekeltfoundation.org>
2011-04-14 14:24:31 +0200

    missing send_one_sms method for vumi backend

--- src/txtalert/apps/gateway/backends/vumi/backend.py
+++ src/txtalert/apps/gateway/backends/vumi/backend.py
@@ -9,23 +9,26 @@ class Gateway(object):
     
     def __init__(self, username, password):
         self.client = Client(username, password)
+    
+    def send_one_sms(self, user, msisdn, smstext):
+        resp = self.client.send_sms(to_msisdn=msisdn, from_msisdn='0',
+                    message=smstext).pop()
+        send_sms = SendSMS()
+        send_sms.user = user
+        send_sms.msisdn = msisdn
+        send_sms.smstext = smstext
+        send_sms.delivery = datetime.now()
+        send_sms.expiry = datetime.now() + timedelta(days=1)
+        send_sms.priority = 'standard'
+        send_sms.receipt = 'Y'
+        send_sms.identifier = str(resp.id)
+        send_sms.save()
+        return send_sms
 
     def send_sms(self, user, msisdns, smstexts):
         responses = []
         for msisdn, smstext in zip(msisdns, smstexts):
-            resp = self.client.send_sms(to_msisdn=msisdn, from_msisdn='0',
-                    message=smstext).pop()
-            send_sms = SendSMS()
-            send_sms.user = user
-            send_sms.msisdn = msisdn
-            send_sms.smstext = smstext
-            send_sms.delivery = datetime.now()
-            send_sms.expiry = datetime.now() + timedelta(days=1)
-            send_sms.priority = 'standard'
-            send_sms.receipt = 'Y'
-            send_sms.identifier = str(resp.id)
-            send_sms.save()
-
+            send_sms = self.send_one_sms(user, msisdn, smstext)
             responses.append(send_sms.pk)
         return SendSMS.objects.filter(pk__in=responses)
 
