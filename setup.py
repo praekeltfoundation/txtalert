6513541835f0358497d04156a447bfe41f1692c3
Simon de Haan <simon@praekeltfoundation.org>
2011-05-24 21:45:29 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 def listify(filename):
-    return filter(None, open(filename,'r').read().split('\n'))
+    return filter(None, open(filename,'r').readlines())
 
 setup(
     name = "txtalert",
     version = "0.1",
-    url = 'http://github.com/praekelt/txtalert',
-    license = 'BSD',
-    description = "txtAlert sends automated, personalized SMS reminders to patients on chronic medication.",
+    url = 'http://github.com/smn/txtalert',
+    license = 'GPL',
+    description = "TxtAlert sends automated, personalized SMS reminders to patients on chronic medication.",
     long_description = open('README.rst', 'r').read(),
     author = 'Praekelt Foundation',
     author_email = "dev@praekeltfoundation.org",
