e3202cd16b1e8cd170ac9b87f26483b1ef5b918c
Simon de Haan <simon@praekeltfoundation.org>
2011-05-26 09:42:18 +0200

    fixing template name conflict and nginx wireframe location

--- txtalert/core/urls.py
+++ txtalert/core/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
-    (r'^$', TemplateView.as_view(template_name='index.html')),
+    (r'^$', TemplateView.as_view(template_name='home.html')),
 )
