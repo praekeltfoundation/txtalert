33c192a38b0f738a57b7c6db03eb621e84d15efc
Simon de Haan <simon@praekeltfoundation.org>
2011-08-31 12:14:59 +0200

    making sure googledoc tests run as part of the full suite, and using postgres as the default db

--- txtalert/env/settings.py
+++ txtalert/env/settings.py
@@ -30,7 +30,7 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'txtalert_dev',
         'USER': '',
         'PASSWORD': '',
