a64cc0bad63212d2caa842b929d4c0d46463ff87
Simon de Haan <simon@praekeltfoundation.org>
2011-09-06 08:38:16 +0200

    update te_send_reminders call

--- config/cron/te_import_data_and_send_reminders.sh
+++ config/cron/te_import_data_and_send_reminders.sh
@@ -2,5 +2,5 @@
 cd /var/praekelt/txtalert/production/current/txtalert && \
     source ve/bin/activate && \
         ./manage.py te_import_data --settings=environments.production | mail -s "TxtAlert TE import on `date`" dev@praekeltfoundation.org && \
-        ./manage.py te_send_reminders --settings=environments.production | mail -s "TxtAlert TE reminders on `date`" dev@praekeltfoundation.org && \
+        ./manage.py send_reminders --settings=environments.production --group='Temba Lethu' | mail -s "TxtAlert TE reminders for Temba Lethu on `date`" dev@praekeltfoundation.org && \
     deactivate
