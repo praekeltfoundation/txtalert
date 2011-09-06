b5879cf52a0f749d3c1ab999a4faa519a69e26ed
Simon de Haan <simon@praekeltfoundation.org>
2011-09-06 14:04:11 +0200

    cron script fix, adding --user

--- config/cron/import_data_and_send_reminders.sh
+++ config/cron/import_data_and_send_reminders.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/praekelt/txtalert/production/current/txtalert && \
     source ve/bin/activate && \
-        ./manage.py te_import_data --settings=environments.production | mail -s "TxtAlert TE import on `date`" dev@praekeltfoundation.org && \
+        ./manage.py te_import_data --settings=environments.production --user='kumbu' | mail -s "TxtAlert TE import on `date`" dev@praekeltfoundation.org && \
         ./manage.py send_reminders --settings=environments.production --group='Temba Lethu' | mail -s "TxtAlert TE reminders for Temba Lethu on `date`" dev@praekeltfoundation.org && \
         ./manage.py gd_import_data --settings=environments.production | mail -s "TxtAlert GD import on `date`" dev@praekeltfoundation.org && \
         ./manage.py send_reminders --settings=environments.production --group='wrhi group' | mail -s "TxtAlert GD reminders for WrHI Group on `date`" dev@praekeltfoundation.org && \
