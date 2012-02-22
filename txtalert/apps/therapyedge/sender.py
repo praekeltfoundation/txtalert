eaa4de079f70833a66d689e0ce6df214c1cf251c
shaunsephton <shaunsephton@gmail.com>
2012-02-22 09:26:41 +0200

    kill for now to avoid duplicate sending

--- txtalert/apps/therapyedge/sender.py
+++ txtalert/apps/therapyedge/sender.py
@@ -2,6 +2,8 @@ from datetime import date
 from txtalert.apps.therapyedge import reminders
 
 def handle(backend, selected_gateway, sms_receipt_handler, group_name):
-    today = date.today()
-    reminders.all(selected_gateway, [group_name])
-    reminders.send_stats(selected_gateway, [group_name], today)
+    # TODO: I'm killing this for now since send_reminders command is still active.
+    # today = date.today()
+    # reminders.all(selected_gateway, [group_name])
+    # reminders.send_stats(selected_gateway, [group_name], today)
+    pass
