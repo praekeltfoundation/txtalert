72c0607e08b7db21c0e2a391daccf9cc6cde4725
Simon de Haan <simon@praekeltfoundation.org>
2012-04-05 00:36:40 +0200

    adding flag for specifying date for sending reminders

--- txtalert/core/management/commands/send_reminders.py
+++ txtalert/core/management/commands/send_reminders.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from txtalert.apps.general.settings.models import Setting
 from txtalert.apps.therapyedge import reminders
 from datetime import datetime
 from optparse import make_option
@@ -8,22 +7,28 @@ from txtalert.apps import gateway
 import sys
 
 class Command(BaseCommand):
-    
+
     option_list = BaseCommand.option_list + (
         make_option('--gateway', default=settings.SMS_GATEWAY_CLASS, dest='gateway',
             help='Specifies the gateway to use, defaults to settings.SMS_GATEWAY_CLASS.'),
-        make_option('--group', default=None, dest='group', 
+        make_option('--group', default=None, dest='group',
             help='Specifies the group to send reminders for'),
+        make_option('--date', default=None, dest='date',
+            help='Specify the date (DD/MM/YYYY) to send reminders for, defaults to today.'),
     )
     help = "Can be run as a cronjob or directly to send SMS reminders."
-    
+
     def handle(self, *args, **options):
         group_name = options.get('group')
         if not group_name:
             sys.exit('Please provide --group')
 
         backend, selected_gateway, sms_receipt_handler = gateway.load_backend(options['gateway'])
-        today = datetime.now().date()
+        date_string = options.get('date')
+        if date_string:
+            today = datetime.strptime(date_string, '%d/%m/%Y').date()
+        else:
+            today = datetime.now().date()
+
         reminders.all(selected_gateway, [group_name])
         reminders.send_stats(selected_gateway, [group_name], today)
-    
