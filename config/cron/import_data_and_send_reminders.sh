c991b3ce712323d420ea34a9a9e64c986d0141ac
Simon de Haan <simon@praekeltfoundation.org>
2012-07-24 10:23:38 +0200

    redirect stderr to stdout and suppress python user warnings

--- config/cron/import_data_and_send_reminders.sh
+++ config/cron/import_data_and_send_reminders.sh
@@ -3,6 +3,6 @@ cd /var/praekelt/txtalert/production/current/txtalert && \
     source ve/bin/activate && \
         ./manage.py te_import_data --settings=environments.production --user='kumbu' | mail -s "TxtAlert TE import on `date`" dev@praekeltfoundation.org && \
         ./manage.py send_reminders --settings=environments.production --group='Temba Lethu' | mail -s "TxtAlert TE reminders for Temba Lethu on `date`" dev@praekeltfoundation.org && \
-        ./manage.py gd_import_data --settings=environments.production | mail -s "TxtAlert Google Docs import on `date`" txtalert-wrhi@praekeltfoundation.org && \
+        python -W"ignore" ./manage.py gd_import_data --settings=environments.production 2>&1 | mail -s "TxtAlert Google Docs import on `date`" txtalert-wrhi@praekeltfoundation.org && \
         ./manage.py send_reminders --settings=environments.production --group='wrhi group' | mail -s "TxtAlert GD reminders for WrHI Group on `date`" dev@praekeltfoundation.org && \
     deactivate
