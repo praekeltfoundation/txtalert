1f25f82994d17f0f5af9318c638cccd142f1fa0f
Simon de Haan <simon@praekeltfoundation.org>
2014-01-23 20:47:18 +0200

    fix paths for cron job

--- config/cron/import_data_and_send_reminders.sh
+++ config/cron/import_data_and_send_reminders.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd /var/praekelt/txtalert/production/current/txtalert && \
+cd /var/praekelt/txtalert && \
     source ve/bin/activate && \
-        ./manage.py te_import_data --settings=environments.production --user='kumbu' | mail -s "TxtAlert TE import on `date`" dev@praekeltfoundation.org && \
-        ./manage.py send_reminders --settings=environments.production --group='Temba Lethu' | mail -s "TxtAlert TE reminders for Temba Lethu on `date`" dev@praekeltfoundation.org && \
-        python -W"ignore" ./manage.py gd_import_data --settings=environments.production 2>&1 | mail -s "TxtAlert Google Docs import on `date`" txtalert-wrhi@praekeltfoundation.org && \
-        ./manage.py send_reminders --settings=environments.production --group='wrhi group' | mail -s "TxtAlert GD reminders for WrHI Group on `date`" dev@praekeltfoundation.org && \
+        ./manage.py te_import_data --settings=production_settings --user='kumbu' | mail -s "TxtAlert TE import on `date`" dev@praekeltfoundation.org && \
+        ./manage.py send_reminders --settings=production_settings --group='Temba Lethu' | mail -s "TxtAlert TE reminders for Temba Lethu on `date`" dev@praekeltfoundation.org && \
+        python -W"ignore" ./manage.py gd_import_data --settings=production_settings 2>&1 | mail -s "TxtAlert Google Docs import on `date`" txtalert-wrhi@praekeltfoundation.org && \
+        ./manage.py send_reminders --settings=production_settings --group='wrhi group' | mail -s "TxtAlert GD reminders for WrHI Group on `date`" dev@praekeltfoundation.org && \
     deactivate
