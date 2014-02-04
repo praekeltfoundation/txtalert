4d8553db5b37f61602f6cd53f5fedc5b21955b25
Simon de Haan <simon@praekeltfoundation.org>
2014-02-04 19:01:20 +0200

    test for listing & saving of patient info

--- txtalert/core/tests/test_clinic_admin.py
+++ txtalert/core/tests/test_clinic_admin.py
@@ -6,10 +6,13 @@ from django.contrib.auth.models import User, Group
 from django.test import TestCase, RequestFactory
 
 from txtalert.core.models import Visit, Clinic, Patient, MSISDN
-from txtalert.core.clinic_admin import VisitAdmin
+from txtalert.core.clinic_admin import VisitAdmin, PatientAdmin
 
 
 class ClinicAdminTestCase(TestCase):
+
+    model_class = None
+    model_admin_class = None
 
     def setUp(self):
         self.admin_user = User.objects.create_superuser(
@@ -34,7 +37,7 @@ class ClinicAdminTestCase(TestCase):
             name='Clinic 2', user=self.clinic2_user, te_id=uuid4().hex)
 
         self.site = AdminSite()
-        self.visit_admin = VisitAdmin(Visit, self.site)
+        self.model_admin = self.model_admin_class(self.model_class, self.site)
 
     def create_patients(self, clinic, count=10):
         patients = []
@@ -70,6 +73,9 @@ class ClinicAdminTestCase(TestCase):
 
 class VisitAdminTestCase(ClinicAdminTestCase):
 
+    model_class = Visit
+    model_admin_class = VisitAdmin
+
     def test_clinic_queryset_filter(self):
         clinic1_patients = [p.te_id for p in
                             self.create_patients(self.clinic1, count=2)]
@@ -82,9 +88,9 @@ class VisitAdminTestCase(ClinicAdminTestCase):
         request.session = {}
 
         self.assertTrue(all([v.patient.te_id in clinic1_patients for v in
-                             self.visit_admin.queryset(request)]))
+                             self.model_admin.queryset(request)]))
         self.assertFalse(any([v.patient.te_id in clinic2_patients for v in
-                             self.visit_admin.queryset(request)]))
+                             self.model_admin.queryset(request)]))
 
     def test_superuser_queryset(self):
         clinic1_patients = self.create_patients(self.clinic1, count=2)
@@ -99,7 +105,7 @@ class VisitAdminTestCase(ClinicAdminTestCase):
 
         self.assertEqual(
             all_patients,
-            set([v.patient.te_id for v in self.visit_admin.queryset(request)]))
+            set([v.patient.te_id for v in self.model_admin.queryset(request)]))
 
     def test_clinic_user_form_listing(self):
         request_factory = RequestFactory()
@@ -107,10 +113,80 @@ class VisitAdminTestCase(ClinicAdminTestCase):
         request.user = self.clinic1_user
         request.session = {}
 
-        form = self.visit_admin.get_form(request)()
+        form = self.model_admin.get_form(request)()
         self.assertTrue("Clinic 1" in str(form['clinic']))
         self.assertFalse("Clinic 2" in str(form['clinic']))
 
 
 class PatientAdminTestCase(ClinicAdminTestCase):
-    pass
+
+    model_class = Patient
+    model_admin_class = PatientAdmin
+
+    def test_patient_queryset_filter(self):
+        clinic1_patients = [p.te_id for p in
+                            self.create_patients(self.clinic1, count=2)]
+        clinic2_patients = [p.te_id for p in
+                            self.create_patients(self.clinic2, count=2)]
+
+        request_factory = RequestFactory()
+        request = request_factory.get('/')
+        request.user = self.clinic1_user
+        request.session = {}
+
+        self.assertTrue(all([patient.te_id in clinic1_patients for patient in
+                             self.model_admin.queryset(request)]))
+        self.assertFalse(any([patient.te_id in clinic2_patients for patient in
+                             self.model_admin.queryset(request)]))
+
+    def test_superuser_queryset(self):
+        clinic1_patients = self.create_patients(self.clinic1, count=2)
+        clinic2_patients = self.create_patients(self.clinic2, count=2)
+        all_patients = set([
+            p.te_id for p in (clinic1_patients + clinic2_patients)])
+
+        request_factory = RequestFactory()
+        request = request_factory.get('/')
+        request.user = self.admin_user
+        request.session = {}
+
+        self.assertEqual(
+            all_patients,
+            set([patient.te_id for patient in
+                 self.model_admin.queryset(request)]))
+
+    def test_clinic_save_model(self):
+
+        [patient] = self.create_patients(self.clinic1, count=1)
+        self.assertEqual(patient.owner, self.clinic1.user)
+
+        other_clinic1_user = User.objects.create_user(
+            'otherclinic1', 'otherclinic1@clinic1.com',
+            password='otherclinic1')
+        other_clinic1_user.groups = [self.clinic1_group]
+        other_clinic1_user.save()
+
+        request_factory = RequestFactory()
+        request = request_factory.get('/')
+        request.user = other_clinic1_user
+        request.session = {}
+
+        self.model_admin.save_model(request, patient, None, None)
+
+        patient = Patient.objects.get(pk=patient.pk)
+        self.assertEqual(patient.owner, other_clinic1_user)
+
+    def test_superuser_save_model(self):
+
+        [patient] = self.create_patients(self.clinic1, count=1)
+        self.assertEqual(patient.owner, self.clinic1.user)
+
+        request_factory = RequestFactory()
+        request = request_factory.get('/')
+        request.user = self.admin_user
+        request.session = {}
+
+        self.model_admin.save_model(request, patient, None, None)
+
+        patient = Patient.objects.get(pk=patient.pk)
+        self.assertEqual(patient.owner, self.clinic1.user)
