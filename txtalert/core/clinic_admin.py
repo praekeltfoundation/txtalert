05a82252a382aa4deb5dc3529ffa5b5bafd728b7
Simon de Haan <simon@praekeltfoundation.org>
2014-02-13 23:04:58 +0200

    simple admin for txtalert

--- txtalert/core/clinic_admin.py
+++ txtalert/core/clinic_admin.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.contrib.admin import ModelAdmin
 from django.contrib.admin.sites import AdminSite
 from django.contrib.auth.models import User
@@ -14,6 +16,38 @@ site = AdminSite(name="clinic-admin")
 class VisitAdmin(ModelAdmin):
     date_hierarchy = 'date'
     form = autocomplete_light.modelform_factory(Visit)
+
+    search_fields = ['patient__te_id', 'te_visit_id']
+
+    fields = [
+        'patient',
+        'date',
+        'clinic',
+        'status',
+        'te_visit_id',
+        'visit_type',
+        'comment',
+    ]
+
+    list_display = [
+        'patient',
+        'clinic',
+        'date',
+        'status',
+        'visit_type',
+    ]
+
+    list_filter = [
+        'date',
+        'status',
+        'visit_type',
+    ]
+
+    list_editable = [
+        'date',
+        'status',
+        'visit_type',
+    ]
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "clinic":
@@ -34,10 +68,79 @@ class VisitAdmin(ModelAdmin):
         return qs.filter(
             patient__owner__in=users_in_same_group_as(request.user))
 
+    def save_model(self, request, obj, form, change):
+        if not obj.te_visit_id:
+            obj.te_visit_id = uuid4().hex
+        return super(VisitAdmin, self).save_model(
+            request, obj, form, change)
+
 
 class PatientAdmin(ModelAdmin):
+
+    readonly_fields = ('owner', 'msisdns')
     date_hierarchy = 'created_at'
     form = autocomplete_light.modelform_factory(Patient)
+
+    list_display = [
+        'te_id',
+        'get_display_name',
+        'active_msisdn',
+        'age',
+        'regiment',
+        'sex',
+        'opted_in',
+        'disclosed',
+        'last_clinic',
+        'next_visit',
+        'last_visit',
+        'language',
+        'updated_at',
+    ]
+
+    list_filter = [
+        'opted_in',
+        'disclosed',
+        'deceased',
+        'last_clinic',
+        'language',
+        'created_at',
+        'updated_at',
+    ]
+
+    list_editable = [
+        'opted_in',
+        'disclosed',
+        'active_msisdn',
+        'language',
+        'sex',
+        'regiment',
+    ]
+
+    def get_display_name(self, obj):
+        return obj.get_display_name()
+    get_display_name.short_description = 'Name'
+
+    def next_visit(self, obj):
+        try:
+            visit = obj.next_visit()
+            return '%s @ %s' % (visit.get_status_display(), visit.date)
+        except (IndexError, ValueError):
+            return None
+
+    def last_visit(self, obj):
+        try:
+            visit = obj.last_visit()
+            return '%s @ %s' % (visit.get_status_display(), visit.date)
+        except (IndexError, ValueError):
+            return None
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "last_clinic":
+            if not request.user.is_superuser:
+                kwargs['queryset'] = Clinic.objects.filter(
+                    user__in=users_in_same_group_as(request.user))
+        return super(PatientAdmin, self).formfield_for_foreignkey(
+            db_field, request, **kwargs)
 
     def queryset(self, request):
         qs = super(PatientAdmin, self).queryset(request)
