17e1267a0e02c5f98a7048a6e6ef4ff40b731031
Simon de Haan <simon@praekeltfoundation.org>
2014-04-01 11:15:47 +0200

    google docs is dead \o/

--- utils/import_data_and_send_reminders.sh
+++ utils/import_data_and_send_reminders.sh
@@ -3,6 +3,5 @@ cd /var/praekelt/txtalert && \
     source ve/bin/activate && \
         ./manage.py te_import_data --user='kumbu' | mail -s "TxtAlert TE import on `date`" simon@praekeltfoundation.org && \
         ./manage.py send_reminders --group='Temba Lethu' | mail -s "TxtAlert TE reminders for Temba Lethu on `date`" simon@praekeltfoundation.org && \
-        python -W"ignore" ./manage.py gd_import_data 2>&1 | mail -s "TxtAlert Google Docs import on `date`" txtalert-wrhi@praekeltfoundation.org && \
         ./manage.py send_reminders --group='wrhi group' | mail -s "TxtAlert GD reminders for WrHI Group on `date`" simon@praekeltfoundation.org && \
     deactivate
