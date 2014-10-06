aa53fac47c4c2244490034eebcdc0d263a289199
Simon de Haan <simon@praekeltfoundation.org>
2014-10-06 11:45:12 +0200

    enable production tasks (used to run in cron)

--- txtalert/tasks.py
+++ txtalert/tasks.py
@@ -7,10 +7,9 @@ def qa_schedule():
     management.call_command('send_reminders', group='QA Group')
 
 
-# NOTE: this is not in use yet, production still uses cron
-#
-# @task
-# def production_schedule():
-#     management.call_command('te_import_data', user='kumbu')
-#     management.call_command('send_reminders', group='Temba Lethu')
-#     management.call_command('send_reminders', group='wrhi group')
+@task
+def production_schedule():
+    management.call_command('te_import_data', user='kumbu')
+    management.call_command('send_reminders', group='Temba Lethu')
+    management.call_command('send_reminders', group='wrhi group')
+
