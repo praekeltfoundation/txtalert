4264444731087193611c5ed66b40688811c2944c
retrorabbitWD <wdawson@retrorabbit.co.za>
2015-01-14 13:39:47 +0200

    Allow clinic to be blank and set default to None

--- txtalert/core/models.py
+++ txtalert/core/models.py
@@ -120,7 +120,7 @@ class AuthProfile(models.Model):
 class MessageType(models.Model):
     """A type of message sent to a patient"""
     group = models.ForeignKey('auth.Group', null=True)
-    clinic = models.ForeignKey('core.Clinic', null=True)
+    clinic = models.ForeignKey('core.Clinic', default=None, null=True, blank=True)
     language = models.ForeignKey('core.Language')
     name = models.CharField(max_length=255)
     message = models.TextField()
